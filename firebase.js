22883206f093f9b76c6157db7ae2f04aac9c8486
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-06-14 23:33:52 +0200

    db config updated

--- firebase.js
+++ firebase.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR API KEY",
-  authDomain: "YOUR AUTH DOMAIN",
-  projectId: "YOUR PROJECT ID",
-  storageBucket: "YOUR BUCKET",
-  messagingSenderId: "YOUR ID",
-  appId: "YOUR APP ID",
+  apiKey: process.env.YOUR_API_KEY,
+  authDomain:  process.env.YOUR_AUTH_DOMAIN,
+  projectId:  process.env.YOUR_PROJECT_ID,
+  storageBucket:  process.env.YOUR_BUCKET,
+  messagingSenderId:  process.env.YOUR_ID,
+  appId:  process.env.YOUR_APP_ID,
 };
-
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 const storage = getStorage();
